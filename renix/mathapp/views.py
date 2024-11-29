d92d69dea331f817972f380d6af765dece32920f
asinrenix <renixasin@gmail.com>
2024-11-29 18:33:46 +0530

    Success

--- renix/mathapp/views.py
+++ renix/mathapp/views.py
@@ -1,19 +1,30 @@
-from django.shortcuts import render 
-def power(request): 
-    context={} 
-    context['power'] = "0" 
-    context['I'] = "0" 
-    context['R'] = "0" 
-    if request.method == 'POST': 
-        print("POST method is used")
-        I =float( request.POST.get('intensity','0'))
-        R = float(request.POST.get('resistance','0'))
-        print('request=',request) 
-        print('intensity=',I) 
-        print('resistance=',R) 
-        power = (((I)**2) * (R))
-        context['power'] = power 
+from django.shortcuts import render
+def power_calculate(request):
+    context = {}
+    context['power'] = ""
+    context['I'] = ""
+    context['R'] = ""  
+
+    if request.method == 'POST':
+
+        I = float(request.POST.get('intensity', '0')) 
+
+        R = float(request.POST.get('resistance', '0')) 
+        
+        power = (I*I)*R
+       
+        context['power'] = f"{ power:.2f}"
+        
         context['I'] = I
-        context['R'] = R 
-        print('Power=',power) 
-    return render(request,'mathapp/math.html',context)
+        
+        context['R'] = R
+        
+        print(f"POST method is used")
+        print(f"request= {request}")
+        print(f"Intensity = {I}")
+        print(f"Resistance = {R}")
+        print(f"Power = {power}")
+       
+      
+    
+    return render(request, 'mathapp/math.html',context)
--- renix/renix/urls.py
+++ renix/renix/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin 
-from django.urls import path 
-from mathapp import views 
-urlpatterns = [ 
-    path('admin/', admin.site.urls), 
-    path('power/',views.power,name="power"),
-    path('',views.power,name="power")
+from django.contrib import admin
+from django.urls import path
+from mathapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('powerofbulb/',views.power_calculate,name="powerofbulb"),
+    path('',views.power_calculate,name="powerofbulb")
 ]
